b4a8545f63b99f970cf30acc7b97e989cdad443e
johncwshc <jcase4218@gmail.com>
2022-08-04 13:13:35 -0400

    package setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="flaskER",
+    version="0.0.1",
+    author="GeeksforGeeks",
+    author_email="contact@gfg.com",
+    packages=["test_package"],
+    description="Enlighten Radio Gateway",
+    long_description="What's Playing, Management, and more",
+    long_description_content_type="text/markdown",
+    url="https://github.com/johnwshc/EnlightenRadio.git",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=[]
+)
